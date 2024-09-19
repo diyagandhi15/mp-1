ea6b2433b560c12ff744a0930314dd2d93ebc157
Diya Gandhi <diyag15@bu.edu>
2024-09-19 15:09:54 -0400

    no borders for now

--- calc.js
+++ calc.js
@@ -42,4 +42,39 @@ function multiplication() {
     else {
         let output = document.getElementById("output")
         output.style.color = "black"
-        output.innerHTML=String(resu
+        output.innerHTML=String(result);
+    }
+}
+
+function division() {
+    let first = document.getElementById("first-number").value;
+    let second = document.getElementById("second-number").value;
+    let result = Number(first) / Number(second);
+    if (result < 0){
+        let output = document.getElementById("output")
+        output.style.color = "red"
+        output.innerHTML=String(result);
+    }
+    else {
+        let output = document.getElementById("output")
+        output.style.color = "black"
+        output.innerHTML=String(result);
+    }
+}
+
+function power() {
+    let first = document.getElementById("first-number").value;
+    let second = document.getElementById("second-number").value;
+    let result = 1;
+
+    for(let i = 0; i < second; i=i+1) {
+        result = first*result
+    }
+
+    document.getElementById("output").innerHTML=String(result);
+}
+
+function clearInput() {
+    document.getElementById("first-number").value = ""; 
+    document.getElementById("second-number").value = ""; 
+}
