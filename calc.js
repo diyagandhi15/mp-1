c23cf65b412e6968716426c0896b32124db874f6
Diya Gandhi <diyag15@bu.edu>
2024-09-19 12:30:01 -0400

    fixed vertical navbar, still need to add padding to website

--- calc.js
+++ calc.js
@@ -2,28 +2,64 @@ function addition() {
     let first = document.getElementById("first-number").value;
     let second = document.getElementById("second-number").value;
     let result = Number(first) + Number(second);
-    document.getElementById("output").innerHTML=String(result);
+    if (result < 0){
+        let output = document.getElementById("output")
+        output.style.color = "red"
+        output.innerHTML=String(result);
+    }
+    else {
+        let output = document.getElementById("output")
+        output.style.color = "black"
+        output.innerHTML=String(result);
+    }
 }
 
 function subtraction() {
     let first = document.getElementById("first-number").value;
     let second = document.getElementById("second-number").value;
     let result = Number(first) - Number(second);
-    document.getElementById("output").innerHTML=String(result);
+    if (result < 0){
+        let output = document.getElementById("output")
+        output.style.color = "red"
+        output.innerHTML=String(result);
+    }
+    else {
+        let output = document.getElementById("output")
+        output.style.color = "black"
+        output.innerHTML=String(result);
+    }
 }
 
 function multiplication() {
     let first = document.getElementById("first-number").value;
     let second = document.getElementById("second-number").value;
     let result = Number(first) * Number(second);
-    document.getElementById("output").innerHTML=String(result);
+    if (result < 0){
+        let output = document.getElementById("output")
+        output.style.color = "red"
+        output.innerHTML=String(result);
+    }
+    else {
+        let output = document.getElementById("output")
+        output.style.color = "black"
+        output.innerHTML=String(result);
+    }
 }
 
 function division() {
     let first = document.getElementById("first-number").value;
     let second = document.getElementById("second-number").value;
     let result = Number(first) / Number(second);
-    document.getElementById("output").innerHTML=String(result);
+    if (result < 0){
+        let output = document.getElementById("output")
+        output.style.color = "red"
+        output.innerHTML=String(result);
+    }
+    else {
+        let output = document.getElementById("output")
+        output.style.color = "black"
+        output.innerHTML=String(result);
+    }
 }
 
 function power() {
